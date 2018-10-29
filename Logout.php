60e303be300353fe0b677ab0420f73437f397a41
Shobhit Srivastava <z1808617@turing.cs.niu.edu>
2018-10-29 15:25:36 -0500

    Logout Text changed

--- Logout.php
+++ Logout.php
@@ -4,6 +4,7 @@
    unset($_SESSION["password"]);
    unset($_SESSION["valid"]);
    
-   echo 'You have cleaned session';
+   echo 'You have successfully logged out<br/>';
+   echo 'Now you will be redirected to the login page';
    header('Refresh: 2; URL = Login.php');
 ?>
