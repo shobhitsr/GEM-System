39c9ec0d4635b5e928d8d28a42e8ff5db08931c5
Shobhit Srivastava <z1808617@turing.cs.niu.edu>
2018-10-29 14:17:35 -0500

    Logout text changed

--- Logout.php
+++ Logout.php
@@ -3,7 +3,7 @@
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
    
-   echo 'You have successfully logged out\n';
+   echo 'You have successfully logged out<br />';
    echo 'Now you will be redirected to the login page';
    header('Refresh: 2; URL = Login.php');
 ?>
