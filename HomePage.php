4cea5ddd15cd868d889bfb133d81757f029a1e9e
Shobhit Srivastava <shobhitsrivastava248@gmail.com>
2018-10-29 13:35:07 -0500

    rest of the files

---
+++ HomePage.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+
+    if(!$_SESSION['valid'] )
+    {
+        echo 'Invalid Login';
+        header("location:HomePage.php");
+    // session is true and equals to string
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>GEM Systems</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+<body>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Global Entertainment Incorporated</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="Create_New_Artist.php">Create Artist</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+            <li><a href="Logout.php" tile="Logout">Logout</a></li>
+        </ul>
+        </div>
+    </nav>
+    <center>
+        <h1>Home Page</h1>
+    </center>
+<!--   
+<div class="container">
+  <h3>Basic Navbar Example</h3>
+  <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+</div>
+-->
+
+</body>
+</html>
