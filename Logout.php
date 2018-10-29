155b5062a38bb466e127254fb0ed9755f855b878
Vidya Anjur <turquoisebop@gmail.com>
2018-10-29 15:06:52 -0500

    updated logout bug

--- Logout.php
+++ Logout.php
@@ -2,6 +2,7 @@
    session_start();
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
+   unset($_SESSION["valid"]);
    
    echo 'You have cleaned session';
    header('Refresh: 2; URL = Login.php');
