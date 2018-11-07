14d5b5a122935514942d0833acc9dfe7cf69787b
Shobhit Srivastava <z1808617@turing.cs.niu.edu>
2018-11-07 16:42:16 -0600

    Added None Selected to all the selections

--- HomePage.php
+++ HomePage.php
@@ -73,6 +73,7 @@
                                 $prep = $conn->prepare($sql2);
                                 $prep -> execute();
                                 $files = $prep->fetchAll();
+                                echo "<option value=0>None Selected</option>";
                                 foreach($files as $row2)
                                 {
 
